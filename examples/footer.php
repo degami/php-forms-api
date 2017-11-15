1565ce6758d699dad4775c6ce5877be315f309e9
Mirko De Grandis <mdegrandis@rblab.it>
2017-11-15 18:06:15 +0100

    commit

--- examples/footer.php
+++ examples/footer.php
@@ -1,7 +1,17 @@
+<?php
+use Degami\PHPFormsApi as FAPI;
+
+?>
+
     <pre style="font-size:10px;"><?php // print_r($form); ?></pre>
 
+<?php
+    $has_session = FAPI\form_builder::session_present();
+    var_dump($has_session);
+    if ($has_session) : ?>
     <h3>Session Info</h3>
     <pre class="sessioninfo"><?php print_r($_SESSION); ?></pre>
+<?php endif; ?>
 
     <div class="functiontitle">function body</div>
     <pre class="functionbody"><?php print $form->get_definition_body();?></pre>
