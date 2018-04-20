af0656d58960a12a69d8dbf4f2c22df9a2f5bcc2
Mirko De Grandis <degami@gmail.com>
2018-04-20 23:02:41 +0200

    commit

---
+++ src/classes/abstracts/fields/composed_field.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * PHP FORMS API
+ * @package degami/php-forms-api
+ */
+/* #########################################################
+   ####                    FIELDS                       ####
+   ######################################################### */
+
+namespace Degami\PHPFormsApi\Abstracts\Fields;
+
+use Degami\PHPFormsApi\form;
+use Degami\PHPFormsApi\Base\field;
+use Degami\PHPFormsApi\Containers\tag_container;
+
+/**
+ * the composed field class
+ */
+abstract class composed_field extends tag_container {
+  /**
+   * is_a_value hook
+   * @return boolean this is a value
+   */
+  public function is_a_value(){
+    return TRUE;
+  }
+
+  /**
+   * on_add_return overload
+   * @return string 'parent'
+   */
+  public function on_add_return(){
+    return 'parent';
+  } 
+}
